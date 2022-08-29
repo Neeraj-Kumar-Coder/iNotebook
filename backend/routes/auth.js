deedacab869c6c7c24ca781bbb9f7f887247f2db
Neeraj Kumar <nk7075034@gmail.com>
2022-08-29 21:33:38 +0530

    Created a validator and a backend for creating a user and save its data to the backend

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
+const User = require("../models/User");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.json(null);
+router.post("/", [
+    body("name", "Enter a valid name.").isLength({ min: 3 }),
+    body("email", "Enter a valid email.").isEmail(),
+    body("password", "Enter a valid password.").isLength({ min: 5 })
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then((user) => { res.json(user) }).catch((err) => { res.json({ error: "Please enter unique value" }) });
 });
 
 module.exports = router;
