dd41cfbdf19bde9b429ae08239109a2c4f9cb980
Neeraj Kumar <nk7075034@gmail.com>
2023-01-05 20:40:33 +0530

    Created an overlay for the notes to 'view, edit and delete' the notes in the 'Your Notes' section

--- inotebook/src/components/NoteItem.js
+++ inotebook/src/components/NoteItem.js
@@ -9,6 +9,20 @@ const NoteItem = (props) => {
             <h2 className="title">{note.title}</h2>
             <h6 className="date">(Last Edited On: {String(new Date(note.date).toLocaleString())})</h6>
             <p className="description">{note.description}</p>
+            <div className="note-overlay">
+                <div className="note-overlay-item-wrapper">
+                    <img className="note-overlay-item-img" src="./icons/view-icon.png" alt="view-icon"></img>
+                    <span className="note-overlay-item-desc">View</span>
+                </div>
+                <div className="note-overlay-item-wrapper">
+                    <img className="note-overlay-item-img" src="./icons/edit-icon.png" alt="edit-icon"></img>
+                    <span className="note-overlay-item-desc">Edit</span>
+                </div>
+                <div className="note-overlay-item-wrapper">
+                    <img className="note-overlay-item-img" src="./icons/delete-icon.png" alt="delete-icon"></img>
+                    <span className="note-overlay-item-desc">Delete</span>
+                </div>
+            </div>
         </div>
     )
 }
