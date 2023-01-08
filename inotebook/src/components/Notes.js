3b9aeba11fabd1530ce4adfed1e7615c1f7529df
Neeraj Kumar <nk7075034@gmail.com>
2023-01-08 10:57:40 +0530

    Added the title to the section that displays users saved notes (# Checkpoint)

--- inotebook/src/components/Notes.js
+++ inotebook/src/components/Notes.js
@@ -9,13 +9,16 @@ const Notes = () => {
     const colors = ["#FFC93C", "#FF7B54", "#FFB26B", "#0081C9", "#5BC0F8", "#86E5FF", "#FEA1BF", "#E5BA73", "#FFCAC8", "#68B984", "#5F8D4E"];
     const numberOfColors = colors.length;
     return (
-        <div className="notes-container">
-            {
-                notes.map((note, index) => {
-                    return <NoteItem note={note} color={colors[index % numberOfColors]} />
-                })
-            }
-        </div>
+        <>
+            <h1 id="notes-container-title">Your Notes</h1>
+            <div className="notes-container">
+                {
+                    notes.map((note, index) => {
+                        return <NoteItem note={note} color={colors[index % numberOfColors]} />
+                    })
+                }
+            </div>
+        </>
     )
 }
 
