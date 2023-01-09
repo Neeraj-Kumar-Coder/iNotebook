e32e706b3389466b646e9a1af2c8e0a439c014f3
Neeraj Kumar <nk7075034@gmail.com>
2023-01-09 16:19:45 +0530

    Added the AddNote functionality on the client side (server side is remaining) and declared the basic CRUD functions in the NoteState that is to be completed (making API Calls to the backend)

--- inotebook/src/components/AddNote.js
+++ inotebook/src/components/AddNote.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import "./AddNote.css"
+import React, { useState, useContext } from 'react';
+import "./AddNote.css";
+import NoteContext from '../context/notes/noteContext';
 
 const AddNote = () => {
+    const [note, setNote] = useState({ title: "", description: "", tag: "" });
+    const context = useContext(NoteContext);
+    const { addNote, updateNode, deleteNote } = context;
+
+    const onChange = (event) => {
+        setNote({ ...note, [event.target.name]: event.target.value });
+    }
+
+    const handleSave = (event) => {
+        event.preventDefault();
+        addNote(note);
+    }
+
     return (
         <div className="add-note">
             <h1 id="add-note-title">Take Your Notes Here</h1>
             <form id="note-form">
                 <div className="note-form-item-wrapper">
                     <label htmlFor="title">Title</label>
-                    <input type={"text"} name="title" id="title" placeholder="Give Your Note A Title"></input>
+                    <input type="text" name="title" id="title" placeholder="Give Your Note A Title" onChange={onChange}></input>
                 </div>
                 <div className="note-form-item-wrapper">
                     <label htmlFor="description">Description</label>
-                    <textarea type={"text"} name="description" id="description" rows={10} cols={30} placeholder="Write Something That You Want To Note"></textarea>
+                    <textarea type="text" name="description" id="description" rows={10} cols={30} placeholder="Write Something That You Want To Note" onChange={onChange}></textarea>
                 </div>
                 <div className="note-form-item-wrapper">
-                    <button type="submit" className="btn" id="save-note-btn"></button>
+                    <button type="submit" className="btn" id="save-note-btn" onClick={handleSave}></button>
                     <button type="reset" className="btn" id="clear-note-btn"></button>
                 </div>
             </form>
--- inotebook/src/context/notes/NoteState.js
+++ inotebook/src/context/notes/NoteState.js
@@ -38,102 +38,28 @@ const NoteState = (props) => {
             "tag": "Personal",
             "date": "2022-12-26T14:00:15.774Z",
             "__v": 0
-        },
-        {
-            "_id": "63a9a8ef4ac83720f5076204",
-            "user": "6311e0e065197c0f4b76594c",
-            "title": "Blue Card",
-            "description": "What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment.",
-            "tag": "Personal",
-            "date": "2022-12-26T14:00:15.774Z",
-            "__v": 0
-        },
-        {
-            "_id": "63a9a8ef4ac83720f5076204",
-            "user": "6311e0e065197c0f4b76594c",
-            "title": "Blue Card",
-            "description": "What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment.",
-            "tag": "Personal",
-            "date": "2022-12-26T14:00:15.774Z",
-            "__v": 0
-        },
-        {
-            "_id": "63a9a8ef4ac83720f5076204",
-            "user": "6311e0e065197c0f4b76594c",
-            "title": "Blue Card",
-            "description": "What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment.",
-            "tag": "Personal",
-            "date": "2022-12-26T14:00:15.774Z",
-            "__v": 0
-        },
-        {
-            "_id": "63a9a8ef4ac83720f5076204",
-            "user": "6311e0e065197c0f4b76594c",
-            "title": "Blue Card",
-            "description": "What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment.",
-            "tag": "Personal",
-            "date": "2022-12-26T14:00:15.774Z",
-            "__v": 0
-        },
-        {
-            "_id": "63a9a8ef4ac83720f5076204",
-            "user": "6311e0e065197c0f4b76594c",
-            "title": "Blue Card",
-            "description": "What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment.",
-            "tag": "Personal",
-            "date": "2022-12-26T14:00:15.774Z",
-            "__v": 0
-        },
-        {
-            "_id": "63a9a8ef4ac83720f5076204",
-            "user": "6311e0e065197c0f4b76594c",
-            "title": "Blue Card",
-            "description": "What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment.",
-            "tag": "Personal",
-            "date": "2022-12-26T14:00:15.774Z",
-            "__v": 0
-        },
-        {
-            "_id": "63a9a8ef4ac83720f5076204",
-            "user": "6311e0e065197c0f4b76594c",
-            "title": "Blue Card",
-            "description": "What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment.",
-            "tag": "Personal",
-            "date": "2022-12-26T14:00:15.774Z",
-            "__v": 0
-        },
-        {
-            "_id": "63a9a8ef4ac83720f5076204",
-            "user": "6311e0e065197c0f4b76594c",
-            "title": "Blue Card",
-            "description": "What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment.",
-            "tag": "Personal",
-            "date": "2022-12-26T14:00:15.774Z",
-            "__v": 0
-        },
-        {
-            "_id": "63a9a8ef4ac83720f5076204",
-            "user": "6311e0e065197c0f4b76594c",
-            "title": "Blue Card",
-            "description": "What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment.",
-            "tag": "Personal",
-            "date": "2022-12-26T14:00:15.774Z",
-            "__v": 0
-        },
-        {
-            "_id": "63a9a8ef4ac83720f5076204",
-            "user": "6311e0e065197c0f4b76594c",
-            "title": "Blue Card",
-            "description": "What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment.",
-            "tag": "Personal",
-            "date": "2022-12-26T14:00:15.774Z",
-            "__v": 0
         }
     ]
     const [notes, setNotes] = useState(initialNotes);
 
+    // Adding a note
+    const addNote = (note) => {
+        console.log("New Note Added!");
+        setNotes(notes.concat(note));
+    }
+
+    // Updating a note
+    const updateNote = () => {
+        ;
+    }
+
+    // Deleting a note
+    const deleteNote = () => {
+        ;
+    }
+
     return (
-        <NoteContext.Provider value={{ notes, setNotes }}>
+        <NoteContext.Provider value={{ notes, setNotes, addNote, updateNote, deleteNote }}>
             {props.children}
         </NoteContext.Provider>
     )
